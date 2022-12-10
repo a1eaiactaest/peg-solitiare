30de27f8a5c6c6d18ce0810a4410d15a77c436be
Piotrek Rybiec <55894077+a1eaiactaest@users.noreply.github.com>
2022-12-10 13:05:54 +0100

    update board class, translate mouse click

--- Form1.cs
+++ Form1.cs
@@ -2,7 +2,7 @@ namespace PegSolitiare
 {
     public partial class Form1 : Form
     {
-        bool[,] board = new bool[7,7];
+        Board board = new Board((7,7));
 
         public Form1()
         {
@@ -11,28 +11,20 @@ namespace PegSolitiare
 
         private void pictureBox1_Paint(object sender, PaintEventArgs e)
         {
-            Graphics g = e.Graphics; 
+            Graphics g = e.Graphics;
 
-            int n = board.GetLength(0);
-            int m = board.GetLength(1);
+            // Draw Board
+            int square_size = pictureBox1.Height / board.shape.Item2;
+            board.Draw(g, square_size, pictureBox1.Width, pictureBox1.Height);
 
-            int box_size = pictureBox1.Height / n;
-
-            for (int i = 0; i <= n; i++)
-            {
-                g.DrawLine(Pens.Black, 0, box_size * i, pictureBox1.Width, box_size * i);
-            }
-
-            for (int i =0; i <= m; i++)
-            {
-                g.DrawLine(Pens.Black, box_size * i, 0, box_size * i, pictureBox1.Height);
-            }
         }
 
         private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
-        {       
-            int x_map = e.X;
-            int y_map = e.Y;
+        {
+            int square_size = pictureBox1.Height / board.shape.Item2;
+
+            int x_map = e.X/square_size;
+            int y_map = e.Y/square_size;
 
             
             //board[x_map, y_map] = true;

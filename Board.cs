840c3c487e80d96ca9c17181de9c26e5d3d47db3
Piotrek Rybiec <55894077+a1eaiactaest@users.noreply.github.com>
2022-12-10 13:05:31 +0100

    move board into a class

---
+++ Board.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PegSolitiare
+{
+    internal class Board
+    {
+        public (int, int) shape = (7, 7);
+
+        public Board((int, int) shape)
+        {
+            this.shape = shape;
+        }
+
+        public void Draw(Graphics g, int square_size, int width_limit, int height_limit)
+        {
+            for (int i = 0; i <= shape.Item1; i++)
+            {
+                g.DrawLine(Pens.Black, 0, square_size * i, width_limit, square_size * i);
+            }
+
+            for (int i = 0; i <= shape.Item2; i++)
+            {
+                g.DrawLine(Pens.Black, square_size * i, 0, square_size * i, height_limit);
+            }
+        }
+    }
+}

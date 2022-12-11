870e3ba2fafb8d0581a4c81814c84c7ad2c20317
Piotrek Rybiec <55894077+a1eaiactaest@users.noreply.github.com>
2022-12-11 20:15:50 +0100

    globalise stuff

--- Board.cs
+++ Board.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -11,18 +13,18 @@ namespace PegSolitiare
 {
     internal class Board
     {
+        public int[] exclude = { 1, 2, 6, 7, 8, 9, 13, 14, 36, 37, 41, 42, 43, 44, 48, 49 };
         public bool[,] state;
         public (int, int) shape;
 
         public Board(bool[,] state)
         {
             this.state = state;
-            shape = (state.GetLength(0), state.GetLength(1));
+            this.shape = (state.GetLength(0), state.GetLength(1));
         }
+
         public void Draw(Graphics g, int square_size)
-        {
-            // this is really dumb, find a better way!
-            int[] exclude = { 1, 2, 6, 7, 8, 9, 13, 14, 36, 37, 41, 42, 43, 44, 48, 49 };
+        {            
             int i = 1;
             for (int row = 0; row < shape.Item1; row++)
             {

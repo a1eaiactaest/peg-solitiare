daf8db26a6a75a1ec2699fefad0b2c01d03b1e89
Piotrek Rybiec <55894077+a1eaiactaest@users.noreply.github.com>
2022-12-11 17:33:29 +0100

    draw pegs

--- Board.cs
+++ Board.cs
@@ -29,17 +29,35 @@ namespace PegSolitiare
                 for (int col = 0; col < shape.Item2; col++)
                 {
                     if (!(exclude.Contains(i))){
+                        // set default value of state
                         state[row,col] = true;
+
+                        // draw board square
                         Rectangle rect = new Rectangle(col * square_size, row * square_size, square_size, square_size);
                         Pen pen = new Pen(Color.Black, 1);
                         g.DrawRectangle(pen, rect);
                     }
 
+                    if (state[row,col] == true)
+                    {
+                        if (!(row == 3 && col == 3))
+                        {
+                            Rectangle rect = new Rectangle((col * square_size)+5, (row * square_size)+5, square_size-10, square_size-10);
+                            Pen pen = new Pen(Color.Red);
+                            g.FillEllipse(Brushes.Red, rect);
+                        }
+                    }
+
                     i += 1;
                 }
             }
-            // empty middle item
+            // empty middle item of state
             state[shape.Item1 / 2, shape.Item2 / 2] = false;
+        }
+
+        public void Update(Graphics g)
+        {
+
         }
     }
 }

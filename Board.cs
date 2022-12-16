209e9e0bb73aebcc89d8f0137403057da13ab822
Piotrek Rybiec <55894077+a1eaiactaest@users.noreply.github.com>
2022-12-16 21:28:12 +0100

    shit load of errors

--- Board.cs
+++ Board.cs
@@ -19,15 +19,23 @@ namespace PegSolitiare
         public Button[,] pegs = new Button[7, 7];
         public (int, int) shape;
 
+        bool src_pick = true;
+
+        int src_row = 0;
+        int src_col = 0;
+
+        int dest_row = 0;
+        int dest_col = 0;
+
         public Board(int[,] state, int square_size, PictureBox pictureBox)
         {
             this.state = state;
             this.shape = (state.GetLength(0), state.GetLength(1));
 
             int i = 1;
-            for (int col = 0; col < shape.Item1; col++)
+            for (int row = 0; row < shape.Item1; row++)
             {
-                for (int row = 0; row < shape.Item2; row++)
+                for (int col = 0; col < shape.Item2; col++)
                 {
 
                     if (!(exclude.Contains(i)))
@@ -70,8 +78,50 @@ namespace PegSolitiare
             Button PegClicked = (Button) sender;
 
             string repr = PegClicked.Name;
-            System.Diagnostics.Debug.WriteLine($"{repr}");
+            //System.Diagnostics.Debug.WriteLine($"{repr}");
 
+            int clicked_row = int.Parse(repr.Substring(1, 1));
+            int clicked_col = int.Parse(repr.Substring(3, 1));
+
+            System.Diagnostics.Debug.WriteLine($"{src_pick}");
+
+            if (src_pick)
+            {
+                src_row = clicked_row;
+                src_col = clicked_col;
+                src_pick = false;
+
+            } else
+            {
+                dest_row = clicked_row;
+                dest_col = clicked_col;
+                src_pick = true;
+            }
+
+            if (src_pick)
+            {
+                (int, int) move_src = (src_row, src_col);
+                (int, int) move_dest = (dest_row, dest_col);
+
+                Move move = new Move(move_src, move_dest, state);
+                if (move.isLegit())
+                {
+                    MakeMove(move);
+                }
+            }
+        }
+
+        public void MakeMove(Move move)
+        {
+
+            state[move.src.Item1, move.src.Item2] = 0;
+            pegs[move.src.Item1, move.src.Item2].BackColor = Color.White;
+
+            state[move.jumpedPegRow, move.src.Item2] = 0; 
+            pegs[move.jumpedPegRow, move.src.Item2].BackColor = Color.White;
+
+            state[move.dest.Item1, move.dest.Item2] = 1; 
+            pegs[move.dest.Item1, move.dest.Item2].BackColor = Color.Black;
         }
 
 
---
+++ Move.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PegSolitiare
+{
+    internal class Move
+    {
+        public (int, int) src;
+        public (int, int) dest;
+        private int[,] state;
+        public int jumpedPegRow;
+
+        public Move((int, int) src, (int,int) dest, int[,] state)
+        {
+            this.src = src;
+            this.dest = dest;
+            this.state = state;
+        }
+
+        public bool isLegit()
+        {
+            /* Transalte:
+             * src.Item1 -> src.row
+             * src.Item2 -> src.col
+            */ 
+
+
+            System.Diagnostics.Debug.WriteLine($"Validating a Move from {src.Item1} {src.Item2} to {dest.Item1} {dest.Item2}:");
+
+            if (src.Item2 == dest.Item2)
+            {
+                if (Math.Abs(src.Item1 - dest.Item1) == 2)
+                {
+                    this.jumpedPegRow = (src.Item1 + src.Item1) / 2;
+
+
+                    // src is a peg
+                    if (state[src.Item1, src.Item2] == 1)
+                    {
+                        // middle is a peg
+                        if (state[jumpedPegRow, src.Item2] == 1)
+                        {
+                            // dest is not a peg
+                            if (state[dest.Item1, dest.Item2] == 0)
+                            {
+                                System.Diagnostics.Debug.WriteLine("Move is valid.");
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+            System.Diagnostics.Debug.WriteLine("Move is not valid.");
+            return false;
+        }
+    }
+}

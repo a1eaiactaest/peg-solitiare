840bd67ea6d5fb5bac05a85c29e4fb8e72c58f4b
Piotrek Rybiec <55894077+a1eaiactaest@users.noreply.github.com>
2022-12-21 22:19:12 +0100

    generate dirs

--- Utils.cs
+++ Utils.cs
@@ -17,10 +17,23 @@ namespace PegSolitiare
                 for (int col = 0; col <= array.GetLength(1); col++)
                 {
                     retList.Add(array[row, col]);
-                }   
+                }
             }
 
             return retList.ToArray();
+        }
+
+        public static Dictionary<string, (int,int)> generate_dirs()
+        {
+            Dictionary<string, (int, int)> hash_dirs = new Dictionary<string, (int, int)>();
+
+            // (row, col)
+            hash_dirs.Add("N", (-1, 0));
+            hash_dirs.Add("S", (1, 0));
+            hash_dirs.Add("W", (0, -1));
+            hash_dirs.Add("E", (0, 1));
+
+            return hash_dirs;
         }
     }
 }

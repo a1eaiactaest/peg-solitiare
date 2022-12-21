c908e87d922579871eb7078c95441f8dfcf08773
Piotrek Rybiec <55894077+a1eaiactaest@users.noreply.github.com>
2022-12-21 21:30:10 +0100

    crate backtrack class

---
+++ Backtrack.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PegSolitiare
+{
+    internal class Backtrack
+    {
+        public Board board;
+        public BitArray state;
+
+        public Backtrack(Board board, BitArray state)
+        {
+            this.board = board;
+            this.state = state;
+        }
+
+        public void Solve()
+        {
+
+        }
+    }
+}

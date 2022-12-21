428169cf765b2a9df9fa69dcd1da574e71d368ed
Piotrek Rybiec <55894077+a1eaiactaest@users.noreply.github.com>
2022-12-21 21:30:39 +0100

    create utils, flatten 2d array

---
+++ Utils.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PegSolitiare
+{
+    public static class Utils
+    {
+        public static int[] flatten(int[,] array)
+        {
+            List<int> retList = new List<int>();
+
+            for (int row = 0; row <= array.GetLength(0); row++)
+            {
+                for (int col = 0; col <= array.GetLength(1); col++)
+                {
+                    retList.Add(array[row, col]);
+                }   
+            }
+
+            return retList.ToArray();
+        }
+    }
+}
